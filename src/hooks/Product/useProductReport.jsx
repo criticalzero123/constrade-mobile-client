c44fa1409c6f56b38458a98419a9470f04302856
criticalzero123 <jdcaramonte159@gmail.com>
2023-03-28 19:12:24 +0800

    ADD: Get List of product price

--- src/hooks/Product/useProductReport.jsx
+++ src/hooks/Product/useProductReport.jsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { reportProduct } from "../../../redux/actions/productActions";
 
-export default function useProductReport() {
+export default function useProductReport(itemName) {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (itemName === undefined) return;
+  }, [itemName]);
 
   const productReport = (info) => {
     dispatch(reportProduct(info));
