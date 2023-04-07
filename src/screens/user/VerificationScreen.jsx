21d7f78d8de7b7786649db48b64eda2af33445eb
criticalzero123 <jdcaramonte159@gmail.com>
2023-04-08 01:33:01 +0800

    Check User Verification Status

--- src/screens/user/VerificationScreen.jsx
+++ src/screens/user/VerificationScreen.jsx
@@ -10,14 +10,31 @@ import React, { useState } from "react";
 import ContainerSafeView from "../../components/CustomViews/ContainerSafeView";
 import { Picker } from "@react-native-picker/picker";
 import { ValidIdType } from "../../../service/enums";
-import { submitIdRequest } from "../../../redux/actions/userActions";
-
+import {
+  getHasRequest,
+  submitIdRequest,
+} from "../../../redux/actions/userActions";
+import { useEffect } from "react";
+import HeaderArrow from "../../components/HeaderArrow/HeaderArrow";
+import { useNavigation } from "@react-navigation/native";
 export default function VerificationScreen({ route }) {
   const { data } = route.params;
   const [idNumber, setIdNumber] = useState("");
   const [idType, setIdType] = useState(0);
+  const navigation = useNavigation();
+  const [requestInfo, setRequestInfo] = useState();
 
-  const onPress = () => {
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getHasRequest(data.user.userId);
+
+      setRequestInfo(res);
+    };
+
+    fetch();
+  }, []);
+
+  const onPress = async () => {
     if (idNumber.trim() === "") {
       alert("Put some id number..");
       return;
@@ -33,11 +50,43 @@ export default function VerificationScreen({ route }) {
       validIdNumber: idNumber.toLowerCase(),
     };
 
-    submitIdRequest(info);
+    const res = await submitIdRequest(info);
+
+    if (res) {
+      alert("Successfully submitted!");
+      navigation.dispatch();
+    } else {
+      alert("Not successful submitted!");
+    }
   };
+
+  if (requestInfo && requestInfo.status.toLowerCase() === "accepted")
+    return (
+      <ContainerSafeView>
+        <HeaderArrow headerName="Verification" />
+        <View className="flex-1 items-center justify-center">
+          <Text className="text-lg font-bold">Already Verified</Text>
+        </View>
+      </ContainerSafeView>
+    );
+
+  if (requestInfo && requestInfo.status.toLowerCase() === "pending")
+    return (
+      <ContainerSafeView>
+        <HeaderArrow headerName="Verification" />
+        <View className="flex-1 items-center justify-center">
+          <Text className="text-lg font-bold">Pending Verification</Text>
+          <Text className="text-base">
+            Submitted at{" "}
+            {new Date(requestInfo.requestDate).toLocaleDateString()}
+          </Text>
+        </View>
+      </ContainerSafeView>
+    );
 
   return (
     <ContainerSafeView>
+      <HeaderArrow headerName="Verification" />
       <ScrollView showsVerticalScrollIndicator={false}>
         <Text>Id Number</Text>
         <TextInput
@@ -59,6 +108,11 @@ export default function VerificationScreen({ route }) {
         <Pressable className="mt-5 p-2 border" onPress={onPress}>
           <Text className="text-center">Submit</Text>
         </Pressable>
+        {requestInfo && requestInfo.status.toLowerCase() === "rejected" && (
+          <Text className="text-center text-red-500 my-10 text-lg font-semibold">
+            You have been rejected, please submit again.
+          </Text>
+        )}
       </ScrollView>
     </ContainerSafeView>
   );
