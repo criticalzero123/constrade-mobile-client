1e97353e6720e976dff2c009e3085deb3f629d1f
criticalzero123 <jdcaramonte159@gmail.com>
2023-04-09 12:03:52 +0800

    Fix view in search item product

--- src/screens/Products/AddProduct/AddProductSearchItems.jsx
+++ src/screens/Products/AddProduct/AddProductSearchItems.jsx
@@ -81,114 +81,122 @@ export default function AddProductSearchItems({ route }) {
   return (
     <ContainerSafeView>
       <HeaderArrow headerName={"Search item"} />
-      <TextInput
-        value={search}
-        onChangeText={setSearch}
-        placeholder="Search here..."
-        onSubmitEditing={handleSubmit}
-        className="py-3 bg-gray-200 px-4 mb-4"
-        style={{ borderRadius: 5 }}
-      />
-      {shops && shops.length !== 0 && (
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          style={{ height: height * 0.7 }}
-        >
-          <Text style={{ width: width * 0.7 }} className="mb-2 text-[#087096]">
-            Automated Price for you based on item condition and market value.
-          </Text>
-          {shops.map((shop, index) => (
-            <View
-              key={index}
-              className="my-2 flex-row items-center justify-between"
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search here..."
+          onSubmitEditing={handleSubmit}
+          className="py-3 bg-gray-200 px-4 mb-4"
+          style={{ borderRadius: 5 }}
+        />
+        {shops && shops.length !== 0 && (
+          <View>
+            <Text
+              style={{ width: width * 0.7 }}
+              className="mb-2 text-[#087096]"
             >
-              <View>
-                <Pressable
-                  className="flex-row items-center  mb-2"
-                  onPress={() => Linking.openURL(`${shop.originUrl}`)}
-                >
-                  <Ionicons name="link-outline" size={22} color="gray" />
-                  <Text className="capitalize text-gray-400 text-base font-semibold ml-2">
-                    {shop.shopName}
-                  </Text>
+              Automated Price for you based on item condition and market value.
+            </Text>
+            {shops.map((shop, index) => (
+              <View
+                key={index}
+                className="my-2 flex-row items-center justify-between"
+              >
+                <View>
+                  <Pressable
+                    className="flex-row items-center  mb-2"
+                    onPress={() => Linking.openURL(`${shop.originUrl}`)}
+                  >
+                    <Ionicons name="link-outline" size={22} color="gray" />
+                    <Text className="capitalize text-gray-400 text-base font-semibold ml-2">
+                      {shop.shopName}
+                    </Text>
+                  </Pressable>
+                  <View className="flex-row items-center mb-1">
+                    <Text className="text-sm font-semibold">PHP</Text>
+                    <Text className="text-[#087096] ml-2 font-semibold text-lg">
+                      {shop.value}
+                    </Text>
+                  </View>
+                  <View className="flex-row items-center mb-1">
+                    <MaterialIcons name="info-outline" size={22} color="gray" />
+                    <Text className="ml-2 text-gray-400">Brand new</Text>
+                  </View>
+                </View>
+                <Pressable onPress={() => toggleModal(shop)}>
+                  <MaterialIcons
+                    name="keyboard-arrow-right"
+                    size={24}
+                    color="gray"
+                  />
                 </Pressable>
-                <View className="flex-row items-center mb-1">
+              </View>
+            ))}
+          </View>
+        )}
+        <>
+          {shops && (
+            <View className="flex-1">
+              {param &&
+                param.itemName !== undefined &&
+                (shops.length === 0 ? (
+                  <View className="flex-1">
+                    <Text>No price data exist in our database</Text>
+                    <OwnPrice />
+                  </View>
+                ) : (
+                  <OwnPrice />
+                ))}
+            </View>
+          )}
+
+          <BottomModal
+            setIsVisible={setModalVisible}
+            isVisible={isModalVisible}
+          >
+            {shopInfo && (
+              <View className="w-full">
+                <Text className="text-xl text-gray-500 mb-4 capitalize">
+                  {shopInfo.shopName}
+                </Text>
+
+                <View className="flex-row items-center mb-2">
                   <Text className="text-sm font-semibold">PHP</Text>
                   <Text className="text-[#087096] ml-2 font-semibold text-lg">
-                    {shop.value}
+                    {shopInfo.value}
                   </Text>
                 </View>
-                <View className="flex-row items-center mb-1">
+                <View className="flex-row items-center mb-2">
                   <MaterialIcons name="info-outline" size={22} color="gray" />
                   <Text className="ml-2 text-gray-400">Brand new</Text>
                 </View>
-              </View>
-              <Pressable onPress={() => toggleModal(shop)}>
-                <MaterialIcons
-                  name="keyboard-arrow-right"
-                  size={24}
-                  color="gray"
-                />
-              </Pressable>
-            </View>
-          ))}
-        </ScrollView>
-      )}
-      <>
-        {shops && (
-          <View className="flex-1">
-            {param &&
-              param.itemName !== undefined &&
-              (shops.length === 0 ? (
-                <View className="flex-1">
-                  <Text>No price data exist in our database</Text>
-                  <OwnPrice />
+                <View className="flex-row items-center">
+                  <Ionicons name="link-outline" size={22} color="gray" />
+                  <Pressable
+                    onPress={() => Linking.openURL(`${shopInfo.originUrl}`)}
+                  >
+                    <Text className="ml-2 text-blue-400 underline">Source</Text>
+                  </Pressable>
                 </View>
-              ) : (
-                <OwnPrice />
-              ))}
-          </View>
-        )}
 
-        <BottomModal setIsVisible={setModalVisible} isVisible={isModalVisible}>
-          {shopInfo && (
-            <View className="w-full">
-              <Text className="text-xl text-gray-500 mb-4 capitalize">
-                {shopInfo.shopName}
-              </Text>
-
-              <View className="flex-row items-center mb-2">
-                <Text className="text-sm font-semibold">PHP</Text>
-                <Text className="text-[#087096] ml-2 font-semibold text-lg">
-                  {shopInfo.value}
-                </Text>
-              </View>
-              <View className="flex-row items-center mb-2">
-                <MaterialIcons name="info-outline" size={22} color="gray" />
-                <Text className="ml-2 text-gray-400">Brand new</Text>
-              </View>
-              <View className="flex-row items-center">
-                <Ionicons name="link-outline" size={22} color="gray" />
                 <Pressable
-                  onPress={() => Linking.openURL(`${shopInfo.originUrl}`)}
+                  className="bg-[#CC481F] p-4 mt-10"
+                  style={{ borderRadius: 5 }}
+                  onPress={onHandleProceedWithPrice}
                 >
-                  <Text className="ml-2 text-blue-400 underline">Source</Text>
+                  <Text className="text-center font-semibold text-white ">
+                    Proceed with this price
+                  </Text>
                 </Pressable>
               </View>
-
-              <Pressable
-                className="bg-[#CC481F] p-4 mt-10"
-                style={{ borderRadius: 5 }}
-                onPress={onHandleProceedWithPrice}
-              >
-                <Text className="text-center font-semibold text-white ">
-                  Proceed with this price
-                </Text>
-              </Pressable>
-            </View>
-          )}
-        </BottomModal>
-      </>
+            )}
+          </BottomModal>
+        </>
+      </ScrollView>
     </ContainerSafeView>
   );
 }
